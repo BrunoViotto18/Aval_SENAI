511efc622083d4924d2e249f69a672abe2443f47
Bruno Viotto <brunoviottosantos@gmail.com>
2022-11-30 22:31:45 -0300

    2 telas do front feitas

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,19 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AreaComponent } from './area/area.component';
 import { MainComponent } from './main/main.component';
+import { VendaComponent } from './venda/venda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    AreaComponent
+    AreaComponent,
+    VendaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       { path: '', component: MainComponent },
-      { path: 'area/:area', component: AreaComponent }
+      { path: 'area/:area', component: AreaComponent },
+      { path: 'venda/:veiculoId', component: VendaComponent}
     ])
   ],
   providers: [],
--- Frontend/src/app/area/area.component.ts
+++ Frontend/src/app/area/area.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AreaComponent implements OnInit {
 
+  veiculos: Array<number> = [1, 2, 3, 4, 5]
+
   constructor() { }
 
   ngOnInit(): void {
